190c05c7566becf4d47cab62f2cb455dca7dd85d
Abbasvora <63236112+Abbasvora@users.noreply.github.com>
2022-11-01 21:51:38 +0530

    changes related to part3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,100 +1,127 @@
-import { useState, useEffect } from 'react'
-import './main.css'
-import Filter from './components/filter'
-import Person from './components/person'
-import PersonForm from './components/person-form'
-import Notification from './components/notification'
-import phoneBookServices from './services/phonebook'
+import { useState, useEffect } from 'react';
+import './main.css';
+import Filter from './components/filter';
+import Person from './components/person';
+import PersonForm from './components/person-form';
+import Notification from './components/notification';
+import phoneBookServices from './services/phonebook';
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filterName, setFilterName] = useState('')
-  const [filterPersons, setFilterPersons] = useState([])
-  const [message, setMessage] = useState(null)
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filterName, setFilterName] = useState('');
+  const [filterPersons, setFilterPersons] = useState([]);
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
-    phoneBookServices.getAll().then(data => setPersons(data))
-  }, [])
+    phoneBookServices.getAll().then((data) => setPersons(data));
+  }, []);
 
   const notEqual = (obj1, obj2) => {
-    if (obj1['name'] !== obj2['name']) {
-      return true
+    if (obj1['name'].toLowerCase() !== obj2['name'].toLowerCase()) {
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
   const addPerson = (event) => {
-    event.preventDefault()
-    const differentName = persons.every((person) => notEqual(person, { name: newName }))
+    event.preventDefault();
+    const differentName = persons.every((person) =>
+      notEqual(person, { name: newName })
+    );
     if (differentName) {
       const personObj = {
         name: newName,
-        number: newNumber
-      }
-      phoneBookServices.create(personObj).then(data => setPersons(persons.concat(data)))
-      setMessage({ class: 'success', message: `${newName} added successfully` })
-      setTimeout(() => {
-        setMessage(null)
-      }, 3000)
-
+        number: newNumber,
+      };
+      phoneBookServices
+        .create(personObj)
+        .then((data) => {
+          setPersons(persons.concat(data));
+          setMessage({
+            class: 'success',
+            message: `${newName} added successfully`,
+          });
+          setTimeout(() => {
+            setMessage(null);
+          }, 3000);
+        })
+        .catch((error) => {
+          const errorMessage = error.response.data.error;
+          setMessage({
+            class: 'error',
+            message: errorMessage,
+          });
+          setTimeout(() => {
+            setMessage(null);
+          }, 3000);
+        });
     } else {
-      if (window.confirm(`${newName} is already added to phonebook, replace old number with new one?`))
-        updatePerson()
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace old number with new one?`
+        )
+      )
+        updatePerson();
     }
-    setNewName('')
-    setNewNumber('')
-  }
+    setNewName('');
+    setNewNumber('');
+  };
 
-  const deletePerson = id => {
-    const name = persons.filter(person => id === person.id)[0].name
+  const deletePerson = (id) => {
+    const name = persons.filter((person) => id === person.id)[0].name;
     if (window.confirm(`Delete ${name}`)) {
       phoneBookServices.reqDelete(id).then(() => {
-        const updated = persons.filter(person => id !== person.id)
-        setPersons(updated)
-      })
+        const updated = persons.filter((person) => id !== person.id);
+        setPersons(updated);
+      });
     }
-  }
+  };
 
   const updatePerson = () => {
-    const person = persons.find(person => newName === person.name)
-    const id = person.id
-    const changedPerson = { ...person, number: newNumber }
+    const person = persons.find(
+      (person) => newName.toLocaleLowerCase() === person.name.toLowerCase()
+    );
+    const id = person.id;
+    const changedPerson = { ...person, number: newNumber };
 
     phoneBookServices
       .update(id, changedPerson)
-      .then(data => {
-        setPersons(persons.map(person => id !== person.id ? person : data))
+      .then((data) => {
+        setPersons(persons.map((person) => (id !== person.id ? person : data)));
       })
       .catch((e) => {
-        setMessage({ class: 'error', message: `Information of ${person.name} has been removed from server` })
+        setMessage({
+          class: 'error',
+          message: e.response.data.error,
+        });
         setTimeout(() => {
-          setMessage(null)
-        }, 3000)
-      })
-  }
+          setMessage(null);
+        }, 3000);
+      });
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleFilterChange = (event) => {
     const newPersons = persons.filter((person) => {
-      const lowerName = person.name.toLowerCase()
+      const lowerName = person.name.toLowerCase();
       if (lowerName.startsWith(event.target.value.toLowerCase())) {
-        return true
+        return true;
       } else {
-        return false
+        return false;
       }
-    })
-    setFilterPersons(newPersons)
-    setFilterName(event.target.value)
-  }
+    });
+    setFilterPersons(newPersons);
+    setFilterName(event.target.value);
+  };
 
   return (
     <div>
@@ -102,11 +129,21 @@ const App = () => {
       <Notification message={message} />
       <Filter filterValue={filterName} filterChange={handleFilterChange} />
       <h3>add new number</h3>
-      <PersonForm onSubmit={addPerson} nameValue={newName} nameChnage={handleNameChange} numberValue={newNumber} numberChnage={handleNumberChange} />
+      <PersonForm
+        onSubmit={addPerson}
+        nameValue={newName}
+        nameChnage={handleNameChange}
+        numberValue={newNumber}
+        numberChnage={handleNumberChange}
+      />
       <h3>Numbers</h3>
-      <Person persons={persons} filterPersons={filterPersons} deletePerson={deletePerson} />
+      <Person
+        persons={persons}
+        filterPersons={filterPersons}
+        deletePerson={deletePerson}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part2/phonebook/src/components/notification.js
+++ part2/phonebook/src/components/notification.js
@@ -1,14 +1,8 @@
 const Notification = ({ message }) => {
   if (message === null) {
-    return null
+    return null;
   }
+  return <div className={message.class}>{message.message}</div>;
+};
 
-  console.log(message)
-  return (
-    <div className={message.class}>
-      {message.message}
-    </div>
-  )
-}
-
-export default Notification
+export default Notification;
